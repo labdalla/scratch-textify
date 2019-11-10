2c8287cbdf060ffd2871aa02783334ea3d6414d8
Lena Abdalla <labdalla@Lenas-MacBook-Pro-4.local>
2019-11-09 20:49:42 -0500

    forgot to change to the correct csv file in index-pool.js

--- index-pool.js
+++ index-pool.js
@@ -8,7 +8,7 @@ const workerpool = require('workerpool');
 const BATCH_SIZE = 1000
 
 // Get all projects and split into batches
-const project_uri = path.resolve(__dirname, 'ids/project_ids.csv');
+const project_uri = path.resolve(__dirname, 'ids/project_ids_last_496001_ids.csv');
 const projects = fs.readFileSync(project_uri, 'utf8').split('\n');
 const batch_list = _.chunk(projects, BATCH_SIZE);
 
