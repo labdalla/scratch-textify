def6cb3018d762f8351d723a6f142f4f1f6ad593
Lena Abdalla <labdalla@Lenas-MacBook-Pro-4.local>
2019-11-09 20:43:22 -0500

    adding pool scripts and id file for remaining project ids to be textified

---
+++ index-pool.js
@@ -0,0 +1,33 @@
+const _ = require('lodash');
+const fs = require('fs');
+const parse = require('./parse_projects_pool.js');
+const path = require('path');
+const workerpool = require('workerpool');
+
+// Settings and Configuration
+const BATCH_SIZE = 1000
+
+// Get all projects and split into batches
+const project_uri = path.resolve(__dirname, 'ids/project_ids.csv');
+const projects = fs.readFileSync(project_uri, 'utf8').split('\n');
+const batch_list = _.chunk(projects, BATCH_SIZE);
+
+
+const pool = workerpool.pool({
+  maxWorkers: 10,
+  maxQueueSize: 1024
+});
+
+for (let i in batch_list) {
+    var filename = "batch_" + i.toString()
+    pool.exec(parse, [batch_list[i], filename])
+        .then(function (result) {
+          console.log('result', result);
+        })
+        .catch(function (err) {
+          console.error(err);
+        })
+        .then(function () {
+          pool.terminate(); // terminate all workers when done
+        });
+}
